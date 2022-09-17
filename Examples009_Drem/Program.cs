b22df06c85bbf4d764827fe1b2c7c93577cb2982
BogdS <Bogdanovas85@gmail.com>
2022-09-17 03:12:01 +0300

    Большой проект

---
+++ Examples009_Drem/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число!");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число!");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b * b == a)
+{ 
+    Console.WriteLine("Первое число является квадратом второго ");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго ");
+}
+
+if (a * a == b)
+{ 
+    Console.WriteLine("Второе число является квадратом первого ");
+}
+else
+{
+    Console.WriteLine("Второе число не является квадратом первого ");
+}
