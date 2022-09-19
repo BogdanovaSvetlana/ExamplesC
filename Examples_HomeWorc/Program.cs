aacd85b1db432718832a0024f9ce5ba443a490a5
BogdS <Bogdanovas85@gmail.com>
2022-09-20 01:28:31 +0300

    Тренировка решения задач

---
+++ Examples_HomeWorc/Program.cs
@@ -0,0 +1,51 @@
+﻿// void firsttest()
+// {
+//    Console.WriteLine("Hello, World!!!");
+// }
+
+// firsttest();
+// firsttest();
+// firsttest();
+
+
+void firsttest(int n)
+{
+   for (int i = 1; i <= n; i++)
+      {
+          Console.WriteLine($"{i} - Hello, World!!!");
+      }
+}
+
+firsttest(10);
+
+
+
+// void printNumbers()
+// {
+//    for (int i = 1; i <= 100; i++)
+//       {
+//           Console.WriteLine(i);
+//       }
+// }
+
+// printNumbers();
+
+
+// void printNumbers(int n)
+// {
+//    for (int i = 1; i <= n; i++)
+//       {
+//          Console.WriteLine(i);
+//       }
+// }
+
+// printNumbers(3);
+// printNumbers(5);
+
+
+// int myNumber()
+// {
+//    return 42;
+// }
+
+// Console.WriteLine(myNumber()*2);
